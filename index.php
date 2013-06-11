8bdcfe78e4edf2aa28a95e1e74ae8fbfa1600a8e
gigahertzz <epicccc@mail.ru>
2013-06-11 12:44:48 +0300

    index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php require_once "start.php" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+  <head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+		<div id="wrapper">
+			<?php require_once "blocks/header.php" ?>
+			<?php require_once "blocks/content.php" ?>
+			<?php require_once "blocks/footer.php" ?>
+		</div>
+	</body>
+</html>
